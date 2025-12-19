53b709d6d5825b61adaf2077b554d95aac608756
saibalaji28 <sbsaibalaji2830@gmail.com>
2025-12-19 15:10:12 +0530

    updated toute.html and metro/views.py

--- metro/views.py
+++ metro/views.py
@@ -3,9 +3,22 @@ from metro.models import Station
 
 def route_view(request):
     stations = Station.objects.all().order_by("name")
+    result = None
+
+    if request.method == "POST":
+        source = request.POST.get("source")
+        destination = request.POST.get("destination")
+
+        result = {
+            "source": source,
+            "destination": destination
+        }
+
     return render(request, "metro/route.html", {
-        "stations": stations
+        "stations": stations,
+        "result": result
     })
+
 
 def map_view(request):
     return render(request, "metro/map.html")
