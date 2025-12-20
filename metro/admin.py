cfb332be45964c4549442328844de41ad89b948c
saibalaji28 <sbsaibalaji2830@gmail.com>
2025-12-20 23:28:49 +0530

    made migration of models, created superuser and data added in admin

--- metro/admin.py
+++ metro/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Station, Line, Connection
 
-# Register your models here.
+admin.site.register(Station)
+admin.site.register(Line)
+admin.site.register(Connection)
--- metro/models.py
+++ metro/models.py
@@ -8,19 +8,24 @@ class Station(models.Model):
     def __str__(self):
         return self.name
 
-
-class Connection(models.Model):
-    from_station = models.ForeignKey(
-        Station,
-        related_name="from_connections",
-        on_delete=models.CASCADE
-    )
-    to_station = models.ForeignKey(
-        Station,
-        related_name="to_connections",
-        on_delete=models.CASCADE
-    )
-    distance = models.IntegerField(help_text="Distance in minutes")
+class Line(models.Model):
+    name = models.CharField( max_length=50)
+    color = models.CharField(max_length=30)
 
     def __str__(self):
-        return f"{self.from_station} → {self.to_station} ({self.distance})"
+        return self.name
+class Connection(models.Model):
+    from_station = models.ForeignKey(
+        Station, related_name="from_connections", on_delete=models.CASCADE
+    )
+    to_station = models.ForeignKey(
+        Station, related_name="to_connections", on_delete=models.CASCADE
+    )
+    line = models.ForeignKey(
+        Line, on_delete=models.CASCADE,
+        null=True, blank=True   # temporary during setup
+    )
+    distance = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.from_station} → {self.to_station} ({self.line})"
